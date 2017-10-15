ce37e5c2e406783d059404c7d304a2b105a46f14
zhang huaifeng <huaifengzhang@outlook.com>
2017-10-15 22:01:15 +0800

    TRACK data and calculate the number of each class

--- run_IBC_rec.m
+++ run_IBC_rec.m
@@ -1,4 +1,4 @@
-function clusters=run_IBC_rec(T,seg_acc,tra_acc,iter_times,s_map)
+function [clusters,cla_num]=run_IBC_rec(T,seg_acc,tra_acc,iter_times,s_map)
 
 tree=IBC(T,seg_acc,tra_acc,s_map);
 clusters=struct('clus_num',0,'clus',struct('id',0,'cla',[]));
@@ -12,5 +12,12 @@ for i=2:iter_times
     clusters=clusters1;
 end
 
+
+[~,cla_w]=size(clusters.clus);
+cla_num=zeros(cla_w,1);
+for i=1:cla_w
+    [cla_num(i,1),~]=size(clusters.clus(i).cla);
+end
+cla_num=sort(cla_num,'descend');
 
 end
